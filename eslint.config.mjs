810278677516b48266df7fcf1b32ded6734ad92c
Kirk Waiblinger <53019676+kirkwaiblinger@users.noreply.github.com>
2025-12-22 12:49:40 -0800

    tweak

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'eslint/config'
 
 export default defineConfig(
   js.configs.recommended,
-  // Including the subsequent config this lint run due to JS rules running on JSON.
+  // Including the subsequent config crashes the lint run due to JS rules running on JSON.
+  // Play with commenting/uncommenting this.
   {
     files: ["src/**/*.json"]
   }
